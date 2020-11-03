5a30e69848de079950a91bfdee1860a89c8f1753
sin-dorei <228827120@qq.com>
2020-11-03 03:10:53 +0000

    修复注册登录导航切换显示

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -22,12 +22,31 @@
             </ul>
 
             <ul class="navbar-nav">
+                @guest
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('login') }}">登录</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('register') }}">注册</a>
                 </li>
+                @else
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="http://qi6mcm0mn.hd-bkt.clouddn.com/images/23d8489a2165ab94a40b98cc0db3b5f8.jpeg" class="img-responsive img-circle" width="30px" height="30px">
+                        {{ Auth::user()->name }}
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">个人中心</a>
+                        <a class="dropdown-item" href="#">编辑资料</a>
+                        <div class="dropdown-divider"></div>
+                        <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button class="dropdown-item" type="submit">退出</button>
+                        </form>
+                    </div>
+                </li>
+                @endguest
             </ul>
         </div>
     </div>
